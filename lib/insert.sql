6311df37623d72cd05b48105684495114830fbf5
cpan1995 <cpan1995@gmail.com>
2021-08-12 17:14:48 -0400

    commit tree faa2057c1872f447dc2d38640e25413441881b7c

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "Black", "asshole", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "Black", "asshole", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "M", "Black", "asshole", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "Black", "asshole", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "M", "Black", "asshole", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "M", "Black", "asshole", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy",5, "M", "Black", "asshole", true);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (5, "M", "Black", "asshole", true);
+-- INSERT INTO bears (age, sex, color, temperament, alive) VALUES (5, "M", "Black", "asshole", "true")
